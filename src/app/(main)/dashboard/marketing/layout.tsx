04a1e0f26a712acce1b14b5a16c6fc70628f096b
jesuss_fl <jesussflr@gmail.com>
2024-06-19 08:16:48 -0400

    fix: minor fix

--- src/app/(main)/dashboard/marketing/layout.tsx
+++ src/app/(main)/dashboard/marketing/layout.tsx
@@ -1,7 +1,6 @@
 import { validateSectionsAndPermissions } from '@/lib/data/validate-permissions'
 import { SECTION_NAMES } from '@/utils/constants/sidebar-constants'
 import { redirect } from 'next/navigation'
-import { PostStoreProvider } from './lib/providers'
 export default async function Layout({
   children,
 }: {
